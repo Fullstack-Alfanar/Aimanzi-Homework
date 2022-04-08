1325abd53b966dc7900d3e356015d2bfd346522a
ZIDANE Aiman PCC ISRAEL <zidanaima1@ferrero.com>
2022-04-08 12:12:25 +0300

    adding files

---
+++ Function's Exercise/java/java.js
@@ -0,0 +1,147 @@
+// Q1 :
+
+// function privetname() {
+//     let firstname, lastname;
+//     firstname = prompt("inser your first name");
+//     lastname = prompt("inser your last name");
+//     alert("your name is : " +firstname +" " +lastname);
+// }
+// privetname();
+
+// -----------------------------------------------------------------------
+
+// Q2:
+
+// function avarage(num1, num2, num3) {
+//     let avr = 0;
+//     avr = (num1 + num2 + num3) / 3;
+//     alert(avr);
+// }
+// // A:
+// // avarage(1, 2, 3);
+// // B : random number from 1-100 .
+// num1 = Math.floor(Math.random() * 101);
+// num2 = Math.floor(Math.random() * 101);
+// num3 = Math.floor(Math.random() * 101);
+// avarage(num1, num2, num3);
+// C:
+// num1 = +prompt("enter number");
+// num2 = +prompt("enter number");
+// num3 = +prompt("enter number");
+// avarage(num1, num2, num3);
+
+// -----------------------------------------------------------------------
+
+// Q3 :
+
+// function size() {
+//     for (let j = 1; j < number; j++)
+//         console.log(rows());
+//         alert(x);
+// }
+// console.log(size());
+
+// -----------------------------------------------------------------------
+
+// Q4 :
+
+// let number, x = "";
+// number = +prompt("insert number");
+// function limit() {
+//     for (j = 1; j <= i; j++)
+//         x += j;
+//     x += ("\n");
+// }
+// function size() {
+//     for (i = 1; i <= number; i++)
+//         console.log(limit());
+//     alert(x)
+// }
+// console.log(size());
+
+// -----------------------------------------------------------------------
+
+// Q5 :
+
+// let number, x = "";
+// number = +prompt("insert number");
+// function limit() {
+//     for (j = 1; j <= i; j++)
+//         x += "*";
+//     x += ("\n");
+// }
+// function size() {
+//     for (i = number; i >= 1; i--)
+//         console.log(limit());
+//     alert(x)
+// }
+// console.log(size());
+// -----------------------------------------------------------------------
+
+// Q6:
+
+// let number, x = "";
+// number = +prompt("insert number");
+// function limit() {
+//     for (j = 1; j <= i; j++)
+//         x += j;
+//     for (j = 1; j <= i; j++)
+//         x += "*";
+//     x += ("\n");
+// }
+// function size() {
+//     for (i = number; i >= 1; i--)
+//         console.log(limit());
+//     alert(x)
+// }
+// console.log(size());
+
+// -----------------------------------------------------------------------
+
+// Q7 :
+
+// let number, x = "";
+// number = +prompt("insert number");
+// function limit() {
+//     for (j = 1; j <= i; j++)
+//         x += "*";
+//     x += ("\n");
+// }
+// function size() {
+//     for (i = number; i >= 1; i--)
+//         console.log(limit());
+// }
+// function size1() {
+//     for (i = 1; i <= number; i++)
+//         console.log(limit());
+//     alert(x)
+// }
+// console.log(size());
+// console.log(size1());
+
+// -----------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Function's Homework/javascript/javas.js
@@ -0,0 +1,166 @@
+// Function"s Homework
+
+// Q1 :
+// let sum = 0;
+// let avr = 0;
+// function avrg(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//     }
+//     avr = sum / arr.length;
+//     return (avr);
+// }
+// console.log(avrg(arr = [1, 2, 3, 4, 5]));
+// console.log(avrg(arr = [2, 4, 6, 8, 10, 12]));
+// console.log(avrg(arr = [1, 2, 3]));
+
+// ----------------------------------------------------------------
+// Q2:
+// function minnum(arr) {
+//     min = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < min) {
+//             min = arr[i];
+//         }
+//     }
+//     return (min);
+// }
+// console.log(minnum(arr = [5, 6, 11, 4, 5]));
+// console.log(minnum(arr = [7, 5, 6, 8, 10, 12]));
+// console.log(minnum(arr = [4, 6, 3]));
+
+// ----------------------------------------------------------------
+// Q3:
+function maxlen(arr) {
+    let max = arr[0].length;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i].length > max) {
+            max = arr[i].length;
+        }
+    }
+    return (max);
+}
+console.log(maxlen(arr = ["hello" ,"its", "me"]));
+console.log(maxlen(arr = ["aiman","zidane"]));
+console.log(maxlen(arr = ["full", "stack","course"]));
+
+// ----------------------------------------------------------------
+
+// Q4:
+// function maxlen(arr) {
+//     let maxword=arr[0];
+//     let max = arr[0].length;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i].length > max) {
+//             max=arr[i].length;
+//             maxword = arr[i];
+//         }
+//     }
+//     return (maxword);
+// }
+// console.log(maxlen(arr = ["m","hello", "its", "me"]));
+// console.log(maxlen(arr = ["xvv","aiman", "zidane"]));
+// console.log(maxlen(arr = ["fv","full", "stack", "course"]));
+
+// ----------------------------------------------------------------
+// Q5:
+// function avrnum(arr) {
+//     let sum = 0, counter = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//     }
+//     avr = sum / arr.length;
+//     for (let j = 0; j < arr.length; j++) {
+//         if (arr[j] >= avr) {
+//             counter++;
+//         }
+//     }
+//     return (counter);
+// }
+// console.log(avrnum(arr = [2, 4, 8, 10]));
+// console.log(avrnum(arr = [7, 5, 6, 8, 10, 12]));
+// console.log(avrnum(arr = [4, 6, 3]));
+// ----------------------------------------------------------------
+
+// Q6.1:
+// function primnum(num) {
+//     let counter = 0;
+//     if (num == 1 || num == 2) {
+//         return true;
+//     } else {
+//         for (let i = 1; i <= num; i++) {
+//             if (num % i == 0) {
+//                 counter++;
+//             }
+//         }
+//         if (counter == 2) {
+//             return (true);
+//         }
+//         else {
+//             return (false);
+//         }
+//     }
+// }
+// console.log(primnum(6));
+// console.log(primnum(4));
+// console.log(primnum(8));
+
+
+// Q6.2: another way :
+
+// function primnum(num) {
+//     let counter = 0;
+//     if (num == 1 || num == 2) {
+//         return true;
+//     } else {
+//         for (let i = 1; i <= num; i++) {
+//             if (num % i == 0) {
+//                 counter++;
+//             }
+//         }
+//         if (counter == 2) {
+//             return (true);
+//         }
+//         else {
+//             return (false);
+//         }
+//     }
+// }
+// console.log(primnum(+prompt("insert number")));
+// console.log(primnum(+prompt("insert number")));
+// console.log(primnum(+prompt("insert number")));
+
+// ----------------------------------------------------------------
+
+// Q7 :
+
+// function primnum(arr) {
+//     let counter = 0, true_count_num = 0, number;
+//     for (let j = 0; j < arr.length; j++)
+//     {
+//         number = arr[j];
+//         for (let i = 1; i <= number; i++)
+//         {
+//             if (number % i == 0)
+//             {
+//                 counter++;
+//             }
+//         }
+//         if (counter == 2)
+//         {
+//             true_count_num++;
+//             counter=0;
+//         }
+//         else 
+//         {
+//             counter=0;
+//             return (false);
+//         }
+//     }
+//     return (true);
+// }
+//   console.log(primnum(arr = [7, 11, 13]));
+//   console.log(primnum(arr = [51, 9, 15]));
+//   console.log(primnum(arr = [21, 23, 29]));
+// ----------------------------------------------------------------
+
