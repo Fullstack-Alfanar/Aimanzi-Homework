349f21ba0e89724c9710d1731909e6065a7b7526
ZIDANE Aiman PCC ISRAEL <zidanaima1@ferrero.com>
2022-05-07 21:32:18 +0300

    adding homework

--- Password validation-v2/javas/script.js
+++ Password validation-v2/javas/script.js
@@ -9,34 +9,32 @@ let pass_special = document.getElementById("special_char");
 
 function checkMatchingFunction() {
 
-    if (password.value == "" || password.value == null) {
-        alert("Enter Your Password");
-    } else {
+    let length = password.value.length >= 8;
+    setIfmatch(length, pass_length);
 
-        let length = password.value.length >= 8;
-        setIfmatch(length, pass_length);
+    let num = password.value.match(/[0-9]/);
+    setIfmatch(num, pass_num);
 
-        let num = password.value.match(/[0-9]/);
-        setIfmatch(num, pass_num);
+    let uppercase = password.value.match(/[A-Z]/);
+    setIfmatch(uppercase, pass_uper);
 
-        let uppercase = password.value.match(/[A-Z]/);
-        setIfmatch(uppercase, pass_uper);
+    let lowercase = password.value.match(/[a-z]/);
+    setIfmatch(lowercase, pass_lower);
 
-        let lowercase = password.value.match(/[a-z]/);
-        setIfmatch(lowercase, pass_lower);
+    let english = password.value.match(/^[a(/^[a-zA-Z0-9\\!\@\#\$\%\^\&\*\?\>\<\,\.\/\;\:\'\"\|\+\=\-\_\;]/);
+    setIfmatch(english, pass_english_let);
 
-        let english = password.value.match(/^[a(/^[a-zA-Z0-9\\!\@\#\$\%\^\&\*\?\>\<\,\.\/\;\:\'\"\|\+\=\-\_\;]/);
-        setIfmatch(english, pass_english_let);
-
-        let special_chara = password.value.match(/[`\~\!\@\#\$\%\^\&\*\(\)\-\_\=\+\,\.\/\'\]\[\<\>\?\"\:\ ]/);
-        setIfmatch(special_chara, pass_special);
-    }
+    let special_chara = password.value.match(/[`\`\~\!\@\#\$\%\^\&\*\(\)\-\_\=\+\,\.\/\'\]\[\<\>\?\"\:\ ]/);
+    setIfmatch(special_chara, pass_special);
 }
-
 
 function setIfmatch(isMatch, element) {
     if (isMatch) {
         element.className = "active";
+        element.innerText = "approval";
+    } else {
+        element.className = "disactive";
+        element.innerText = "require missing";
     }
 }
 
