2952574a20c02df2ede33d7edd930afe0660bb6c
ZIDANE Aiman PCC ISRAEL <zidanaima1@ferrero.com>
2022-06-23 00:37:31 +0300

    adding react homework

---
+++ reacthomework22.6.22/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TextColor from "./components/text color/textcolor.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TextColor />
+      </header>
+    </div>
+  );
+}
+
+export default App;
